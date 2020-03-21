b2d914405213427dbd3b733e57cc5c726c49d018
Frank Siri <franksiri@gmail.com>
2020-03-21 16:47:10 -0400

    new contact page

---
+++ src/pages/contact.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+const ContactPage = () => {
+    return (
+        <div>
+            <h1>Contact Us</h1>
+            <p>Fill in your info and request</p>
+            <p>Need the news ?  
+                <a href="https://www.google.com/" target="_blank">google</a></p>
+        </div>
+    )
+}
+
+export default ContactPage
